7ec99bae58e4621a1aeddc7565505f3e91cd927e
Mark Mulli <markmulli001@gmail.com>
2024-09-25 09:25:09 +0300

    working with Interface(Type Aliases)

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,11 @@
 import { Component, computed, EventEmitter, input, Input, Output, output } from '@angular/core';
 
+interface User {
+  id:string,
+  avatar: string,
+  name:string
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -12,23 +18,25 @@ export class UserComponent {
     // @Input({ required: true }) name!: string
 
     //working with input signals
-    id = input.required<string>()
-    avatar = input.required<string>()
-    name = input.required<string>()
+    // id = input.required<string>()
+    // avatar = input.required<string>()
+    // name = input.required<string>()
+
+    @Input({required: true}) user!: User
 
     // @Output() select = new EventEmitter()
     select = output<string>()
 
-    imagePath = computed(() => {
-      return 'assets/users/' + this.avatar()
-    })
+    // imagePath = computed(() => {
+    //   return 'assets/users/' + this.avatar()
+    // })
 
 
-    // get ImagePath() {
-    //   return "assets/users/" + this.avatar
-    // }
+    get ImagePath() {
+      return "assets/users/" + this.user.avatar
+    }
 
     onSelectUser() {
-      this.select.emit(this.id())
+      this.select.emit(this.user.id)
     }
 }
